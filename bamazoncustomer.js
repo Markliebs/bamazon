1619b9635d901d79436b9866f0c08015e693844f
markliebs <markliebs@me.com>
2016-06-17 00:37:06 -0400

    Attempting to prettify results

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,23 +1,32 @@
-var mysql=require('mysql');
-var inquirer=require('inquirer');
-var connection=mysql.createConnection({
-	host:"localhost",
-	port: 3306,
-	user:"root", //Your username
-	password:"", //Your password
-	database:"bamazon_DB"})
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var utils = require('util');
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root", //Your username
+    password: "", //Your password
+    database: "bamazon_DB"
+})
 
-connection.connect(function(err){
-	if(err) throw err;
-	console.log("Connected to BAMaZon!");})
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Welcome to BAMaZon!")
+    console.log("Here are the available items for sale!")
+        ;
+})
 
 //     connection.query('SELECT * FROM Products', function(err, res){
 //     if(err) throw err;
 //     console.log(res);
 // })
 
-    connection.query('SELECT * FROM Products',function(err,res){
-         console.log("ID | Item Name | Department | Cost | Amount in Stock")
-	for(var i=0;i<res.length;i++){
-		console.log(res[i].itemID+" | "+res[i].ProductName+" | "+res[i].DepartmentName+" | "+res[i].Price+" | "+res[i].StockQuantity);}
-	console.log("-----------------------------------");})
+connection.query('SELECT * FROM Products', function (err, res) {
+    if (err) throw err;
+    console.dir("ID * Item Name | Department | Cost | Amount in Stock"), { colors: true };
+    for (var i = 0; i < res.length; i++) {
+        utils.inspect.styles.string = 'cyan';
+        console.log(res[i].itemID + " * " + res[i].ProductName + " | " + res[i].DepartmentName + " | " + res[i].Price + " | " + res[i].StockQuantity);
+    }
+    console.log("-----------------------------------");
+})
